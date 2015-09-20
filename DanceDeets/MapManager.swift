827928d9b3200f23b7fc91caa8039be91d260d77
Mike Lambert <mlambert@gmail.com>
2015-09-20 03:09:03 -0400

    Start supporting Google Maps for users who have it installed.

--- DanceDeets/MapManager.swift
+++ DanceDeets/MapManager.swift
@@ -8,6 +8,18 @@ public class MapManager
 {
     public class func showOnMap(event: Event) {
         if let coordinate = event.geoloc?.coordinate {
+            if UIApplication.sharedApplication().canOpenURL(NSURL(string: "comgooglemaps://")!) {
+                var encodedVenue:String
+                if event.venue != nil {
+                    encodedVenue = event.venue!.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+                } else {
+                    encodedVenue = "\(coordinate.latitude),\(coordinate.longitude)"
+                }
+
+                let url = "comgooglemaps://?q=\(encodedVenue)&center=\(coordinate.latitude),\(coordinate.longitude)&zoom=15"
+                UIApplication.sharedApplication().openURL(NSURL(string:url)!)
+            }
+
             let placemark = MKPlacemark(coordinate: coordinate, addressDictionary: nil)
             let mapItem:MKMapItem = MKMapItem(placemark: placemark)
             mapItem.openInMapsWithLaunchOptions(nil)
