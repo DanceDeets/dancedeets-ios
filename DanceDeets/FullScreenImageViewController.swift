9dd80b8fd9abbd10cfa64ebb7b08d14b01e6dcd0
Mike Lambert <mlambert@gmail.com>
2015-11-15 01:29:27 -0500

    Add a bit of border around the the image, so that it pans more obviously. Turns out to have some tricky interaction between zoomScale and setContentOffset, that made this harder to accomplish than I thought it'd be.

--- DanceDeets/FullScreenImageViewController.swift
+++ DanceDeets/FullScreenImageViewController.swift
@@ -19,25 +19,35 @@ class FullScreenImageViewController : UIViewController, UIScrollViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         scrollView.delegate = self
         imageView.image = image
         if event != nil {
             AnalyticsUtil.track("View Flyer", withEvent: event!)
         }
 
-        let heightPadding = max(0, (view.frame.height - image!.size.height)/2)
-        let widthPadding = max(0, (view.frame.width - image!.size.width)/2)
+        // The black offset around the image, so that scrolling/zooming is more obvious
+        let outerOffset = CGFloat(30)
+
+        // This centers the image in the scrollview (and the offset addition gives some extra black borders)
+        let heightPadding = max(0, (view.frame.height - image!.size.height)/2) + outerOffset
+        let widthPadding = max(0, (view.frame.width - image!.size.width)/2) + outerOffset
         scrollView.contentInset = UIEdgeInsets(top: heightPadding, left: widthPadding, bottom: heightPadding, right: widthPadding)
 
+        // This sets the default zoom (and max-zoomed-out zoom) to be the image fullscreen in the display
         let scale = min(view.frame.width / imageView.frame.width, view.frame.height / imageView.frame.height)
         scrollView.minimumZoomScale = scale
         scrollView.zoomScale = scale
 
+        // This undoes the offset black borders, so that they initially appear offscreen (but allow zooming)
+        // Note, I believe contentOffset and zoomScale interact, so it needs to be done *after* we set the zoomScale above,
+        // or things don't work properly.
+        scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: false)
+
         tapGesture = UITapGestureRecognizer(target: self, action: "tapped")
         scrollView.addGestureRecognizer(tapGesture!)
     }
-    
+
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
