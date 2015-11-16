ef761e71abc3fbc5df87463743310fac750b3862
Mike Lambert <mlambert@gmail.com>
2015-11-16 05:06:52 -0500

    Clean up unused Utilities functions.

--- DanceDeets/Utilities.swift
+++ DanceDeets/Utilities.swift
@@ -8,32 +8,7 @@
 
 import Foundation
 
-class Utilities{
-    
-    class func heightRequiredForText(text:String, lineHeight:CGFloat, font:UIFont, width:CGFloat)->CGFloat{
-        let nsStr = NSString(string: text)
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.minimumLineHeight = lineHeight
-        paragraphStyle.maximumLineHeight = lineHeight
-        
-        let attributesDictionary:[String:AnyObject] =
-        [NSParagraphStyleAttributeName: paragraphStyle,
-            NSFontAttributeName:font]
-        
-        let bounds =
-        nsStr.boundingRectWithSize(CGSizeMake(width, CGFloat.max), options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: attributesDictionary, context: nil)
-        return bounds.size.height;
-    }
-    
-    class func printFontFamilies()
-    {
-        for name in UIFont.familyNames()
-        {
-                let names = UIFont.fontNamesForFamilyName(name)
-                print(name)
-                print(names)
-        }
-    }
+class Utilities {
 
     class func dateFormatter() -> NSDateFormatter {
         let dateFormatter = NSDateFormatter()
