a2394cd40e76a0ad6e736edac4df3f378ee1d6c9
Mike Lambert <mlambert@gmail.com>
2015-11-23 07:43:07 -0500

    Add explanatory comment.

--- DanceDeets/EventShareButton.swift
+++ DanceDeets/EventShareButton.swift
@@ -12,6 +12,8 @@ import Foundation
 public class EventShareButton: FBSDKShareButton {
 
     func _share(sender: AnyObject) {
+        // We launch the dialog manually, so that we can pass a view controller and use the sharesheet. For more info:
+        // http://stackoverflow.com/questions/33086211/facebook-share-button-on-ios-9-opens-browser-instead-of-native-facebook-app/33871172#33871172
         FBSDKShareDialog.showFromViewController(parentViewController(), withContent: shareContent, delegate: nil)
     }
 }
